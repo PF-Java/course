b3581e4cbe6993c5a756b9811a11f76d5ecce602
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-03 22:12:15 +0700

    add test case and refactoring

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,10 +1,27 @@
 public class CourseTest {
     public static void main(String[] args) {
         Course course = new Course("Java");
-        String trang = "trang";
+        displayCourse(course);
+
+        String trang = "Trang";
         course.addStudent(trang);
-        System.out.println(course.getName());
-        System.out.println(course.getStudents());
-        System.out.println(course.getStudentsCount());
+        displayCourse(course);
+
+        String loan = "Loan";
+        course.addStudent(loan);
+        displayCourse(course);
+
+        String anh = "Anh";
+        course.addStudent(anh);
+        displayCourse(course);
+    }
+
+    public static void displayCourse(Course course) {
+        System.out.println("Course \"" + course.getName() + "\" has " + course.getStudentsCount() + " student(s): {");
+        String[] students = course.getStudents();
+        for (int i = 0; i < course.getStudentsCount(); i++) {
+            System.out.println("\t\"" + students[i] + "\",");
+        }
+        System.out.println("}");
     }
 }
