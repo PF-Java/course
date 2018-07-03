e9df1905b6807e24d42b19784d0c3b7afd387451
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-03 22:22:36 +0700

    pass test case of method removeStudent

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -23,4 +23,21 @@ public class Course {
         students[studentsCount] = name;
         studentsCount++;
     }
+
+    public void removeStudent(String name) {
+        boolean found = false;
+        for (int i = 0; i < studentsCount; i++) {
+            if (students[i] == name) {
+                found = true;
+                for (int j = i + 1; j < studentsCount; j++) {
+                    students[j - 1] = students[j];
+                }
+                studentsCount--;
+                System.out.println("Student \"" + name + "\" removed!");
+            }
+        }
+        if (!found) {
+            System.out.println("Student \"" + name + "\" not existed in list, nothing removed!");
+        }
+    }
 }
